d2ba0b7e81c77c7820d6e0ceab8d18d3e7b16c55
Shawnclq <shawnclq@Shawnclqs-MacBook-Pro.local>
2018-02-07 12:26:31 +0800

    Add Block.java, PostalCode.java, Street.java, Unit.java

---
+++ src/seedu/addressbook/data/person/PostalCode.java
@@ -0,0 +1,58 @@
+package seedu.addressbook.data.person;
+
+import seedu.addressbook.data.exception.IllegalValueException;
+
+/**
+ * Represents an Address's postal code in the address book.
+ * Guarantees: immutable; is valid as declared in {@link #isValidPostalCode(String)}
+ */
+public class PostalCode {
+    public static final String EXAMPLE = "#12-34";
+    public static final String MESSAGE_POSTALCODE_CONSTRAINTS = "Address postal code should contain only numbers";
+    public static final String POSTALCODE_VALIDATION_REGEX = "\\d+";
+
+    public final String value;
+    private boolean isPrivate;
+
+    /**
+     * Validates given phone number.
+     *
+     * @throws IllegalValueException if given postal code string is invalid.
+     */
+    public PostalCode(String postalCode, boolean isPrivate) throws IllegalValueException {
+        this.isPrivate = isPrivate;
+        String trimmedPostalCode = postalCode.trim();
+        if (!isValidPostalCode(trimmedPostalCode)) {
+            throw new IllegalValueException(MESSAGE_POSTALCODE_CONSTRAINTS);
+        }
+        this.value = trimmedPostalCode;
+    }
+
+    /**
+     * Returns true if the given string is a valid address postal code.
+     */
+    public static boolean isValidPostalCode(String test) {
+        return test.matches(POSTALCODE_VALIDATION_REGEX);
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof PostalCode // instanceof handles nulls
+                && this.value.equals(((PostalCode) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+    public boolean isPrivate() {
+        return isPrivate;
+    }
+}
